37f605acb3cb4cdf98404acb0989e47fcf43b198
EXP <289065406@qq.com>
2021-01-21 13:23:04 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@
 
 USER="admin"
 PASS="admin123"
-UID=`id | awk -F '[(=]' '{print $2}'`
-GID=`id | awk -F '[(=]' '{print $4}'`
+U_ID=`id | awk -F '[(=]' '{print $2}'`
+G_ID=`id | awk -F '[(=]' '{print $4}'`
 
 set -- `getopt u:p:i:g: "$@"`
 while [ -n "$1" ]
@@ -18,12 +18,12 @@ do
         shift ;;
     -p) PASS="$2"
         shift ;;
-    -i) UID="$2"
+    -i) U_ID="$2"
         shift ;;
-    -g) GID="$2"
+    -g) G_ID="$2"
         shift ;;
   esac
   shift
 done
 
-username=${USER} password=${PASS} uid=${UID} gid=${GID} docker-compose up -d
+username=${USER} password=${PASS} uid=${U_ID} gid=${G_ID} docker-compose up -d
